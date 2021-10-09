d8c602f26be0fc99f48798e5c0169009259a952b
Phạm Minh Hiếu <pmhieu1409@gmail.com>
2021-10-09 20:45:08 +0700

    update

---
+++ main.js
@@ -0,0 +1,70 @@
+//Bài 1:
+/*
+let inputArr=[];
+
+let n;
+
+do{
+    n=Number(prompt("Nhập vào số mảng: "));
+}
+while(isNaN(n));
+
+console.log(n);
+
+for(let i=0;i<n;i++){
+    inputArr[i] = prompt("Nhập vào chuỗi kí tự thứ " + (i+1) + ":");
+}
+let max=0;
+let newArr=[];
+for(let i=0;i<n;i++){
+    if(max<inputArr[i].length){
+        max=inputArr[i].length;
+    }
+}
+
+for(i=0;i<n;i++){
+    if(inputArr[i].length>=max){
+        newArr.push(inputArr[i]);
+    }
+}
+
+console.log(newArr);
+*/
+
+//Bài 2:
+/*
+let n;
+
+do{
+    n=Number(prompt("Nhập vào số lượng người tham gia: "));
+}
+while(isNaN(n));
+
+let Arr=[];
+
+for(let i=0; i<n;i++){
+    do{
+        Arr[i]=Number(prompt("Nhập vào cân nặng của người thứ " + (i+1) +" :"));
+    }
+    while(isNaN(Arr[i]));
+}
+
+let sum1 = 0, sum2 = 0;
+
+for(let i = 0;i<n;i++){
+    if(i%2===0){
+        sum1+=Arr[i];
+    }
+    if(i%2!==0){
+        sum2+=Arr[i]
+    }
+}
+
+console.log("Tổng cân nặng đội 1 là:", sum1);
+
+console.log("Tổng cân nặng đội 2 là:", sum2);
+*/
+
+
+
+
